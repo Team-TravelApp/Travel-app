d530dcbac825182fb6043c589462e95ad10c4e2e
AlexiosIKomnenos <106697127+AlexiosIKomnenos@users.noreply.github.com>
2022-12-12 22:18:15 -0500

    comments, some templates

--- travel/models.py
+++ travel/models.py
@@ -4,3 +4,19 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Attraction(models.Model): 
+    pass
+
+class Comment(models.Model): 
+    comment_owner = models.ForeignKey(User,on_delete=models.CASCADE, related_name = 'comments')
+    attraction = models.ForeignKey(Attraction,on_delete=models.CASCADE, related_name = 'comments')
+    commenttext = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.text} by:{self.comment_owner} on {self.attraction}'
+
+    class Meta:
+        ordering = ['created_at']
+
