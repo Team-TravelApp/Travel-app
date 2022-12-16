20d14649f72f87032723f1a0c7c1d5ddce4076fd
timcooper709 <timcooper709@gmail.com>
2022-12-16 15:47:43 -0500

    comment serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from travel.models import AttractionPost
+from travel.models import AttractionPost, Comment
 
 
 class AttractionPostSerializer(serializers.ModelSerializer):
@@ -9,3 +9,11 @@ class AttractionPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = AttractionPost
         fields = ['pk', 'user', 'title', 'country', 'description', 'created_at', 'interest_rating', 'comments', 'total_comments']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    comment_owner = serializers.SlugRelatedField(read_only=True, slug_field="username")
+    attractionpost = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Comment
+        fields = ['pk', 'comment_owner', 'attraction', 'commenttext', 'created_at']
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from .serializers import AttractionPostSerializer
-from travel.models import AttractionPost
+from .serializers import AttractionPostSerializer, CommentSerializer
+from travel.models import AttractionPost, Comment
 from rest_framework.viewsets import ModelViewSet
 from django.db.models import Q
 from django.db.models import Count
+from rest_framework.generics import ListCreateAPIView, get_object_or_404
 
 
 class AttractionPost(ModelViewSet):
@@ -37,3 +38,25 @@ class AttractionPost(ModelViewSet):
         #This means that the update will only save if the user is the creator of the post.
         if self.request.user == serializer.instance.user:
             serializer.save()
+
+class CommentListCreateView(ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        #This is how you create a search code pulled form drf docs.
+        search_term = self.request.query_params.get("search")
+        if search_term is not None:
+            #Filtering comment objects for anything that has search term in title.
+            results = Comment.objects.filter(attraction__icontains=self.request.query_params.get("search"))
+            return results
+
+        # this is where we are only getting the answers related to the comment
+        return Comment.objects.filter(comment_id=self.kwargs["comment_pk"])
+    # This is where we are changing the perform / create function within this API view 
+    def perform_create(self, serializer):
+        # This is where we are defining what a comment is, also making sure the comment is saved to the right user and attraction
+        # 
+        comment = get_object_or_404(Comment, pk=self.kwargs["comment_pk"])
+        serializer.save(user=self.request.user, comment=comment)
+        
